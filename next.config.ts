6389203dd6b1298e41e754824ba0a7597ecf1fea
justslam@gmail.com <justslam@gmail.com>
2026-08-16 01:46:05 +0700

    chore: configure turbopack root and re-organize tailwind and development dependencies

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: process.cwd(),
+  },
   allowedDevOrigins: ['192.168.56.1', 'localhost:3000', '192.168.56.1:3000']
 };
 
